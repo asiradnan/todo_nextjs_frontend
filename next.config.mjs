711da4f825aa14c8676cd94247d0f43638202310
asiradnan <asiradnan23@gmail.com>
2025-10-18 02:13:25 +0600

    attempt css fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/todo_nextjs_frontend',
-    assetPrefix: '/todo_nextjs_frontend/',
     trailingSlash: true,
     reactStrictMode: true,
     images: {
